95adb23973bd4e5f814cdb739a0caa98be5d5867
pilaraguilar22 <jpaguilar@fi.uba.ar>
2024-09-22 22:04:24 -0300

    Primer desarrollo de rey, programacion basica con mucho if

---
+++ src/main/java/org.ajedrez/model/Rey.java
@@ -0,0 +1,27 @@
+public class Rey extends Pieza{
+    public Rey(Integer color){
+        super(color);
+    }
+    public boolean validarMovimiento(Tablero tablero, Integer oi, Integer oj, Integer di, Integer dj){
+        if (di >= tablero.getDimensiones() || dj >= tablero.getDimensiones()) {
+            return false;
+        }
+
+        Integer idiff = Math.abs(oi - di);
+        Integer jdiff = Math.abs(oj - dj);
+
+        if((idiff == 1 && jdiff == 1) || (idiff == 1 && jdiff == 0) || (idiff == 0 && jdiff == 1)) && !tablero.estaOcupada(di, dj){
+            return true
+        }
+        if (tablero.estaOcupada(di, dj)) {
+            Pieza piezaDestino = tablero.obtenerPieza(di, dj);
+            return piezaDestino.getColor() != this.getColor();
+        }
+
+        return false;
+    }
+    @Override
+    public Integer getColor() {
+        return this.color;
+    }
+}
