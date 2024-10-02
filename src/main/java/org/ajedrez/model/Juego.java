f05d0ac2ff1c8b554d7c000f4c863689dbbb6962
juampajuan <jdominguezl@fi.uba.ar>
2024-10-02 18:01:23 -0300

    Agregue algunas clases

---
+++ src/main/java/org/ajedrez/model/Juego.java
@@ -0,0 +1,31 @@
+package org.ajedrez.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Juego {
+    private List<Jugador> jugadores;
+    private Tablero tablero;
+    private AdministradorDeMovimientos administradordemovimientos;
+    private AdministradorDeTurnos administradordeturnos;
+
+    public Juego(String nombreJugador1, String nombreJugador2){
+        List<Color> colores = new ArrayList<>();
+        Collections.shuffle(colores);
+        colores.add(Color.NEGRO);
+        colores.add(Color.BLANCO);
+
+        Jugador jugador1 = new Jugador(nombreJugador1,colores.getFirst());
+        Jugador jugador2 = new Jugador(nombreJugador2,colores.getLast());
+
+        this.tablero = new Tablero();
+        this.administradordeturnos = new AdministradorDeTurnos();
+        this.administradordemovimientos = new AdministradorDeMovimientos();
+    }
+
+    public Tablero getTablero(){
+        return this.tablero;
+    }
+
+}
---
+++ src/main/java/org/ajedrez/model/Jugador.java
@@ -0,0 +1,4 @@
+package org.ajedrez.model;
+
+public class Jugadores {
+}
